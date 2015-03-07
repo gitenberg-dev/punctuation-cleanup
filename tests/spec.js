b0cc726feb5a922645c61a89130ec8dee6358519
rsperberg <rsperberg@gmail.com>
2015-03-06 22:11:38 -0500

    initial commit of first tests

---
+++ tests/spec.js
@@ -0,0 +1,34 @@
+/*eslint-env node, mocha */
+var chai = require('chai');
+var assert = chai.assert,
+    expect = chai.expect,
+    should = chai.should();
+
+var replacements = require('../replacements.js');
+var cleanup = require('../cleanup.js');
+
+describe("apostrophes", function() {
+
+    it("should fix don't", function() {
+        var testString = "everlasting fire... don't you feel it? Blind, chockfull of sin! Repent,";  // Conrad 'Narcissus'
+        var tryThis = cleanup(testString);
+        expect(tryThis).to.equal("everlasting fire... don’t you feel it? Blind, chockfull of sin! Repent,")
+   });
+    it("should fix I'd", function() {
+        var testString = "But they'd never met anyone from LoD.  Even some of the";  // Hacker Crackdown
+        var tryThis = cleanup(testString);
+        expect(tryThis).to.equal("But they’d never met anyone from LoD.  Even some of the")
+   });
+    it("should fix he'll", function() {
+        var testString = "He'll shrieve my soul, he'll wash away";  // Rime of the Ancient Mariner
+        var tryThis = cleanup(testString);
+        expect(tryThis).to.equal("He’ll shrieve my soul, he’ll wash away")
+   });
+    it("should fix I'm", function() {
+        var testString = "SCARED of a guy like that.  But I'm not scared.\nI'm from Chicago.  I'm gonna hunt him down.";  // Hacker Crackdown
+        var tryThis = cleanup(testString);
+        expect(tryThis).to.equal("SCARED of a guy like that.  But I’m not scared.\nI’m from Chicago.  I’m gonna hunt him down.")
+   });
+
+});
+//
